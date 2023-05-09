831ffd5b217f47c52a9cfae0fd2fbf1e2187ed85
Abdus Sattar <sasorganicagro@gmail.com>
2023-05-09 19:26:21 +0600

    Update the column name and rollback

--- queries.sql
+++ queries.sql
@@ -8,3 +8,9 @@ SELECT name, escape_attempts FROM animals WHERE weight_kg > 10.5;
 SELECT * FROM animals WHERE neutered = 'true';
 SELECT * FROM animals WHERE name <> 'Gabumon';
 SELECT * FROM animals WHERE weight_kg BETWEEN  10.4 AND 17.3;
+
+BEGIN;
+ALTER TABLE animals RENAME COLUMN SPECIES TO UNSPECIFIED;
+SELECT * FROM ANIMALS;
+ROLLBACK;
+SELECT * FROM ANIMALS;
