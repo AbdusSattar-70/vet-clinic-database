ba35ebfde22be668c2e063641d65fdae40ff9541
Abdus Sattar <sasorganicagro@gmail.com>
2023-05-22 18:59:51 +0600

    Check performance for owners table

--- queries.sql
+++ queries.sql
@@ -144,3 +144,4 @@ ORDER BY AMOUNT_OF_VISITS DESC LIMIT 1;
 explain analyze SELECT COUNT(*) FROM visits where animals_id = 4;
 
 explain analyze SELECT * FROM visits where vets_id = 2;
+EXPLAIN ANALYZE SELECT * FROM owners where email = 'owner_18327@mail.com';
